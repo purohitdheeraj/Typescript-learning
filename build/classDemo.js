a36d5ca56d78bb1ad14cd2de80d9092b156017a1
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2024-02-04 07:11:10 +0530

    tsc

--- build/classDemo.js
+++ build/classDemo.js
@@ -1,33 +1,34 @@
 class Room {
-	constructor(room) {
-		this.room = room;
-		this.room7 = 'guestRoom';
-		this.room8 = 'meditation hall';
-		this.family = [];
-		this.dob = '2022-07-23';
-		this.dob2 = '1999-06-01';
-	}
-	addFamilyMember(member) {
-		if (typeof member === 'string') {
-			this.family.push(member);
-		} else {
-			this.family.push(...member);
-		}
-	}
-	showFamily() {
-		console.log(this.family);
-	}
-	cleanRoom(soap) {
-		console.log(`cleaning ${this.room}'s with  ${soap}`);
-	}
+    constructor(room) {
+        this.room = room;
+        this.room7 = 'guestRoom';
+        this.room8 = 'meditation hall';
+        this.family = [];
+        this.dob = '2022-07-23';
+        this.dob2 = '1999-06-01';
+    }
+    addFamilyMember(member) {
+        if (typeof member === 'string') {
+            this.family.push(member);
+        }
+        else {
+            this.family.push(...member);
+        }
+    }
+    showFamily() {
+        console.log(this.family);
+    }
+    cleanRoom(soap) {
+        console.log(`cleaning ${this.room}'s with  ${soap}`);
+    }
 }
 const dauji = new Room('Dauji');
 dauji.addFamilyMember([
-	'Jagannathji',
-	'Subhadraji',
-	'Dauji',
-	'LaxmiMata',
-	'Sudharshanji',
+    'Jagannathji',
+    'Subhadraji',
+    'Dauji',
+    'LaxmiMata',
+    'Sudharshanji',
 ]);
 dauji.showFamily();
 dauji.cleanRoom('panchamrit');
